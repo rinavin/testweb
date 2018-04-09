717cea264ffaf08a3a60d2d97417128fd3eabc55
Rina Vinokurov <rina_vinokurov@magicsoftware.com>
2018-04-09 15:54:06 +0300

    subform component

--- src/app/magic/src/ui/subform-component.ts
+++ src/app/magic/src/ui/subform-component.ts
@@ -2,25 +2,21 @@
  * Created by rinav on 10/09/2017.
  */
 import {Component, Input, ViewContainerRef} from "@angular/core";
-import {TaskMagicService} from "../services/task.magics.service";
 import {BaseTaskMagicComponent} from "./app.baseMagicComponent";
 
 @Component({
   selector: 'm-subform',
-  providers: [TaskMagicService],
+
   template:    `
-    
-    
-    <ndc-dynamic [magic]='controlId' class="controlId" [ndcDynamicComponent]="Component" [ndcDynamicInputs]="Parameters">
+    <ndc-dynamic  [ndcDynamicComponent]="Component" [ndcDynamicInputs]="Parameters">
     </ndc-dynamic>
 `
 })
 
 //not working for now
 export class Subform {
-
-  @Input() controlId: string;
-
+  id : string;
+  @Input('magic') set magic(val) {this.id = val; };
 
   component: BaseTaskMagicComponent;
 
@@ -29,11 +25,11 @@ export class Subform {
   }
 
   get Component(): Component {
-    return this.component.mgGetComp(this.controlId);
+    return this.component.mgGetComp(this.id);
   }
 
   get Parameters(): any {
-    return this.component.mgGetParameters(this.controlId);
+    return this.component.mgGetParameters(this.id);
   }
 }
 
