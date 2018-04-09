0f5d5a935ed452d5af7ebf82d2dd92dea3c64ac2
Rina Vinokurov <rina_vinokurov@magicsoftware.com>
2018-04-09 17:20:58 +0300

    checkbox component

--- src/app/magic/src/ui/MagicRouterContainer.ts
+++ src/app/magic/src/ui/MagicRouterContainer.ts
@@ -1,5 +1,8 @@
 import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 import {BaseTaskMagicComponent} from './app.baseMagicComponent';
+import {MagicEngine} from '../services/magic.engine';
+import {GuiEvent} from "@magic/engine";
 
 @Component({
   selector: 'app-container',
@@ -13,9 +16,21 @@ export class MagicRouterContainer implements OnInit {
   private MainComp = null;
   private MainCompParameters: any = {};
 
-  constructor(private changeDetectorRef: ChangeDetectorRef) {}
+  constructor(private changeDetectorRef: ChangeDetectorRef,
+              private activatedRoute: ActivatedRoute,
+              protected magic: MagicEngine) {}
 
   ngOnInit() {
+    if (BaseTaskMagicComponent.routeInfo === null) {
+      let guiEvent: GuiEvent = new GuiEvent('RouterNavigate', null, 0);
+      guiEvent.RouterPath = this.activatedRoute.snapshot.routeConfig.path;
+
+      if (this.activatedRoute.snapshot.routeConfig.outlet !== 'primary')
+        guiEvent.RouterOutletName = this.activatedRoute.snapshot.routeConfig.outlet;
+
+      this.magic.insertEvent(guiEvent);
+    }
+
     if (BaseTaskMagicComponent.routeInfo !== null) {
 
       this.MainComp = BaseTaskMagicComponent.componentListBase.getComponents(BaseTaskMagicComponent.routeInfo.formName);
